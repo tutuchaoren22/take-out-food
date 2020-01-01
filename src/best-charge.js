48af65c71582454094c179c68c795df9e77cec84
duyuqian <15738381799@163.com>
2020-01-01 23:45:29 +0800

    [duyuqian] feat: Use tasking to organize ideas and draw pipeline diagrams, and complete the code implementation framework

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,33 @@
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+    var shoppingList = getAllItems(selectedItems);
+    var discount = hasDiscount(shoppingList);
+    var summary = summaryAllItems(shoppingList, discount);
+    var result = printAllItems(shoppingList, discount, summary);
+    console.log(result);
 }
+
+function getAllItems(selectedItems) {
+    var loadAllItems = require('./items');
+    var loadPromotions = require('./promotions');
+    console.log(loadAllItems());
+    console.log(loadPromotions());
+
+
+    return shoppingList;
+}
+
+function hasDiscount(shoppingList) {
+    return discount;
+}
+
+function summaryAllItems(shoppingList, discount) {
+    return summary;
+}
+
+function printAllItems(shoppingList, discount, summary) {
+    return result;
+}
+
+
+let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+bestCharge(inputs);
