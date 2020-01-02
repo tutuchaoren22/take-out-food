9da8c0a38e0e5b0cd2528ae1b5a774015bf5b1a6
duyuqian <15738381799@163.com>
2020-01-02 16:56:00 +0800

    [duyuqian] feat: Complete the function to print all items about name,price,count,discountinformation and total

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,17 +1,11 @@
 function bestCharge(selectedItems) {
     var shoppingList = getAllItems(selectedItems);
-    console.log(shoppingList);
-
-    var discount = hasDiscount(shoppingList);
-    console.log(discount);
-
-    // var summary = summaryAllItems(shoppingList, discount);
-    // var result = printAllItems(shoppingList, discount, summary);
-    // console.log(result);
+    var summary = hasDiscount(shoppingList);
+    var result = printAllItems(shoppingList, summary);
+    return result;
 }
 
 function getAllItems(selectedItems) {
-    const loadAllItems = require('./items');
     const allItems = loadAllItems();
     const [selectedItemsID, selectedItemsCount] = getItemsIdAndCount(selectedItems);
     const shoppingList = selectedItemsID.map(
@@ -32,13 +26,40 @@ function getItemsIdAndCount(selectedItems) {
 }
 
 function hasDiscount(shoppingList) {
-    let hasDiscountType, totalPrice;
-    let [hasDiscountTypeOne, totalPriceTypeOne] = hasDiscountTypeOne(shoppingList);
-    let [hasDiscountTypeTwo, totalPriceTypeTwo] = hasDiscountTypeTwo(shoppingList);
-    if () {
+    let summary = {};
+    summary.hasDiscount = 0;
+    let totalPrice = getTotalPrice(shoppingList);
+    let [hasDiscountOne, totalPriceOne] = hasDiscountTypeOne(totalPrice);
+    let [hasDiscountTwo, totalPriceTwo, discountedItems] = hasDiscountTypeTwo(shoppingList);
+    if (hasDiscountOne === true) {
+        if (hasDiscountTwo === true) {
+            if (totalPriceOne <= totalPriceTwo) {
+                summary.totalPrice = totalPriceOne;
+                summary.hasDiscount = 1;
+                summary.discountPrice = 6;
+            } else {
+                summary.totalPrice = totalPriceTwo;
+                summary.hasDiscount = 2;
+                summary.discountPrice = totalPrice - totalPriceTwo;
+                summary.discountedItems = discountedItems;
+            }
 
+        } else {
+            summary.hasDiscount = 1;
+            summary.totalPrice = totalPriceOne;
+            summary.discountPrice = 6;
+        }
+    } else {
+        if (hasDiscountTwo === true) {
+            summary.hasDiscount = 2;
+            summary.totalPrice = totalPriceTwo;
+            summary.discountPrice = totalPrice - totalPriceTwo;
+            summary.discountedItems = discountedItems;
+        } else {
+            summary.totalPrice = totalPriceOne;
+        }
     }
-    return [hasDiscountType, totalPrice];
+    return summary;
 }
 
 function getTotalPrice(shoppingList) {
@@ -49,8 +70,7 @@ function getTotalPrice(shoppingList) {
     return totalPrice;
 }
 
-function hasDiscountTypeOne(shoppingList) {
-    let totalPrice = getTotalPrice(shoppingList);
+function hasDiscountTypeOne(totalPrice) {
     let hasDiscount = false;
     if (totalPrice >= 30) {
         hasDiscount = true;
@@ -60,32 +80,39 @@ function hasDiscountTypeOne(shoppingList) {
 }
 
 function hasDiscountTypeTwo(shoppingList) {
-    const loadPromotions = require('./promotions');
     const discountItems = loadPromotions()[1].items;
     let hasDiscount = false;
-    shoppingList.forEach(item => {
+    let discountedItems = [];
+    let shoppingListDiscounted = JSON.parse(JSON.stringify(shoppingList));
+    shoppingListDiscounted.forEach(item => {
         if (discountItems.indexOf(item.id) !== -1) {
             item.subTotal *= 0.5;
             hasDiscount = true;
+            discountedItems.push(item.name);
         }
     });
-    let totalPrice = getTotalPrice(shoppingList);
-    return [hasDiscount, totalPrice];
+    let totalPrice = getTotalPrice(shoppingListDiscounted);
+    return [hasDiscount, totalPrice, discountedItems];
 }
 
-function summaryAllItems(shoppingList, discount) {
-    return summary;
-}
+function printAllItems(shoppingList, summary) {
+    let result = `============= 订餐明细 =============\n`;
+    shoppingList.forEach(
+        item => {
+            return result += `${item.name} x ${item.count} = ${item.subTotal}元\n`
 
-function printAllItems(shoppingList, discount, summary) {
+        }
+    );
+    result += `-----------------------------------\n`
+    switch (summary.hasDiscount) {
+        case 1:
+            result += `使用优惠:\n满30减6元，省6元\n-----------------------------------\n`;
+            break;
+        case 2:
+            result += `使用优惠:\n指定菜品半价(${summary.discountedItems.join('，')})，省${summary.discountPrice}元\n-----------------------------------\n`;
+            break;
+    }
+
+    result += `总计：${summary.totalPrice}元\n===================================`;
     return result;
 }
-
-
-let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
-var shoppingList = getAllItems(inputs);
-console.log(shoppingList);
-hasDiscountTypeOne(shoppingList)
-hasDiscountTypeTwo(shoppingList)
-
-// bestCharge(inputs);
